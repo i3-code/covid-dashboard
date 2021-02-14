5ca6ab35beecb4a9f78987f978c753604364555b
i3-code <i3pobox@gmail.com>
2021-02-14 11:27:53 +0300

    refactor: remove online keyboard

--- covid-dashboard/src/components/Left/CountryFilter/CountryFilter.js
+++ covid-dashboard/src/components/Left/CountryFilter/CountryFilter.js
@@ -1,19 +1,11 @@
 import "./CountryFilter.scss";
-import "./kb-style.css";
 import "./icons.css";
 
 import React from "react";
-import Keyboard from "./keyboard";
 export default class CountryFilter extends React.Component {
-  componentDidMount() {
-    const input = document.getElementById("keyboard-input");
-    new Keyboard(document.body, input, this.props.writeFilter);
-  }
-
   render() {
     return (
       <input
-        id="keyboard-input"
         type="text"
         onChange={this.props.onChange}
         placeholder="Search..."
